1e63dccfc9a2a66edb444e22c9e3b598ab227878
njk nkl <baev-lev1@mail.ru>
2022-08-21 20:02:18 +0700

    0.02

--- src/main/java/org/example/controller/SimpleController.java
+++ src/main/java/org/example/controller/SimpleController.java
@@ -1,14 +1,20 @@
 package org.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+@RestController
 public class SimpleController {
 
     @GetMapping("/requests")
-    public Long getRequestsCount() {
-        long result = 1000-7;
-        return result;
+    public String getRequestsCount() {
+        int result = 1000-7;
+        return result+" ";
+    }
+    @RequestMapping(path = "/get", method = RequestMethod.GET)
+    public String test(){
+        return "OK";
     }
 }
